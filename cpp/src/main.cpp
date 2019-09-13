e7fdd089a6e636e218ad18b016864cbe619ecdbc
Andrey Izman <izmanw@gmail.com>
2019-09-13 05:47:45 +0300

    Fix typos

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -9,10 +9,10 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-    uint8_t *crypted = AES256::encrypt((const uint8_t *)string("TEXT").c_str(), 4, (const uint8_t *)string("PASS").c_str());
+    uint8_t *crypted = AES256::encrypt((const uint8_t *)"TEXT", 4, (const uint8_t *)"PASS");
     cout  << crypted << endl;
 
-    uint8_t *decrypted = AES256::decrypt((const uint8_t *)crypted, (const uint8_t *)string("PASS").c_str());
+    uint8_t *decrypted = AES256::decrypt((const uint8_t *)crypted, (const uint8_t *)"PASS");
     cout << decrypted << endl;
 
     return 0;
