7eee091322796f963d25f7ecd72bc02307798705
Andrey Izman <izmanw@gmail.com>
2019-09-18 05:48:35 +0300

    Update copyright header

--- net/src/aes256.cs
+++ net/src/aes256.cs
@@ -1,3 +1,30 @@
+// aes256.cs
+// This file is part of AES-everywhere project (https://github.com/mervick/aes-everywhere)
+//
+// This is an implementation of the AES algorithm, specifically CBC mode,
+// with 256 bits key length and PKCS7 padding.
+//
+// Copyright Andrey Izman (c) 2018-2019 <izmanw@gmail.com>
+// Licensed under the MIT license
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 // <summary>AES256 class implements the OpenSSL compatible cipher AES/256/CBC/PKCS7</summary>
 // <copyright file="aes256.cs" company="Andrey Izman">
 // Copyright (c) 2018 All Rights Reserved
