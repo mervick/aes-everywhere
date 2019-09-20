36c572965a52dbcf346af296a185c56a282547b3
Andrey Izman <izmanw@gmail.com>
2019-09-21 01:11:04 +0300

    Fix tests in python2

--- python/src/tests/test_aes256.py
+++ python/src/tests/test_aes256.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from AesEverywhere.aes256 import aes256
+import base64
+from hashlib import md5
+from Crypto import Random
+from Crypto.Cipher import AES
+# from AesEverywhere import aes256
+# from AesEverywhere.aes256 import aes256
 
 try:
     if sys.version_info < (2, 7):
@@ -11,46 +16,53 @@ try:
 except ImportError:
     import unittest
 
+
+py2 = sys.version_info[0] == 2
+
 FAIL = "Invalid decrypt"
+
+def u(str):
+    return str.decode('utf-8').encode('utf-8') if py2 else str.encode('utf-8')
+
 
 class TestAes256(unittest.TestCase):
 
     def test_decrypt1(self):
         text = aes256.decrypt("U2FsdGVkX1+Z9xSlpZGuO2zo51XUtsCGZPs8bKQ/jYg=", "pass")
-        self.assertEqual(text, "test".encode('utf-8'), FAIL)
+        self.assertEqual(text, u("test"), FAIL)
 
     def test_decrypt2(self):
         text = aes256.decrypt("U2FsdGVkX1+8b3WpGTbZHtd2T9PNQ+N7GqebGaOV3cI=", "Data 😄 текст")
-        self.assertEqual(text, "test".encode('utf-8'), FAIL)
+        self.assertEqual(text, u("test"), FAIL)
 
     def test_decrypt3(self):
         text = aes256.decrypt("U2FsdGVkX18Kp+T3M9VajicIO9WGQQuAlMscLGiTnVyHRj2jHObWshzJXQ6RpJtW", "pass")
-        self.assertEqual(text, "Data 😄 текст".encode('utf-8'), FAIL)
+        self.assertEqual(text, u("Data 😄 текст"), FAIL)
 
     def test_decrypt4(self):
         text = aes256.decrypt("U2FsdGVkX1/O7iqht/fnrFdjn1RtYU7S+DD0dbQHB6N/k+CjzowfC2B21QRG24Gv", "Data 😄 текст")
-        self.assertEqual(text, "Data 😄 текст".encode('utf-8'), FAIL)
+        self.assertEqual(text, u("Data 😄 текст"), FAIL)
 
     def test_encrypt_decrypt1(self):
         text = "Test! @#$%^&*"
         passw = "pass"
         enc = aes256.encrypt(text, passw)
         dec = aes256.decrypt(enc, passw)
-        self.assertEqual(text.encode('utf-8'), dec, FAIL)
+        self.assertEqual(u(text), dec, FAIL)
 
     def test_encrypt_decrypt2(self):
         text = "Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺"
         passw = "pass"
         enc = aes256.encrypt(text, passw)
         dec = aes256.decrypt(enc, passw)
-        self.assertEqual(text.encode('utf-8'), dec, FAIL)
+        self.assertEqual(u(text), dec, FAIL)
 
     def test_encrypt_decrypt3(self):
         text = "Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺"
         passw = "哈罗 こんにちわ Акїў 😺"
         enc = aes256.encrypt(text, passw)
         dec = aes256.decrypt(enc, passw)
-        self.assertEqual(text.encode('utf-8'), dec, FAIL)
+        self.assertEqual(u(text), dec, FAIL)
 
 
 if __name__ == '__main__':
