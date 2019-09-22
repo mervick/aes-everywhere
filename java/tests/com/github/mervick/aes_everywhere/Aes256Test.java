85c7f87327a5fb5dffdcc2e0803998e8b947c2ff
Andrey Izman <izmanw@gmail.com>
2019-09-22 21:35:15 +0300

    Update tests

--- java/tests/com/github/mervick/aes_everywhere/Aes256Test.java
+++ java/tests/com/github/mervick/aes_everywhere/Aes256Test.java
@@ -9,28 +9,23 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Aes256Test
 {
-    protected byte[] encrypt(byte[] in, byte[] pass) throws Exception
-    {
+    protected byte[] encrypt(byte[] in, byte[] pass) throws Exception {
         return Aes256.encrypt(in, pass);
     }
 
-    protected String encrypt(String in, String pass) throws Exception
-    {
+    protected String encrypt(String in, String pass) throws Exception {
         return Aes256.encrypt(in, pass);
     }
 
-    protected byte[] decrypt(byte[] in, byte[] pass) throws Exception
-    {
+    protected byte[] decrypt(byte[] in, byte[] pass) throws Exception {
         return Aes256.decrypt(in, pass);
     }
 
-    protected String decrypt(String in, String pass) throws Exception
-    {
+    protected String decrypt(String in, String pass) throws Exception {
         return Aes256.decrypt(in, pass);
     }
 
-    private void testDecrypt(String in, String pass, String expect)
-    {
+    private void testDecrypt(String in, String pass, String expect) {
         String result = "";
         byte[] bytes = new byte[0];
 
@@ -47,48 +42,30 @@ public class Aes256Test
     }
 
     @Test
-    public void testDecrypt1() throws Exception
-    {
-        testDecrypt(
-            "U2FsdGVkX1+Z9xSlpZGuO2zo51XUtsCGZPs8bKQ/jYg=",
-            "pass",
-            "test"
-        );
+    public void testDecrypt1() throws Exception {
+        testDecrypt("U2FsdGVkX1+Z9xSlpZGuO2zo51XUtsCGZPs8bKQ/jYg=", "pass", "test");
     }
 
     @Test
-    public void testDecrypt2()
-    {
-        testDecrypt(
-            "U2FsdGVkX1+8b3WpGTbZHtd2T9PNQ+N7GqebGaOV3cI=",
-            "Data 😄 текст",
-            "test"
-        );
+    public void testDecrypt2() {
+        testDecrypt("U2FsdGVkX1+8b3WpGTbZHtd2T9PNQ+N7GqebGaOV3cI=", "Data 😄 текст", "test");
     }
 
     @Test
-    public void testDecrypt3()
-    {
-        testDecrypt(
-            "U2FsdGVkX18Kp+T3M9VajicIO9WGQQuAlMscLGiTnVyHRj2jHObWshzJXQ6RpJtW",
-            "pass",
-            "Data 😄 текст"
-        );
+    public void testDecrypt3() {
+        testDecrypt("U2FsdGVkX18Kp+T3M9VajicIO9WGQQuAlMscLGiTnVyHRj2jHObWshzJXQ6RpJtW", "pass", "Data 😄 текст");
     }
 
-    private void testEncryptDecrypt(String in, String pass)
-    {
+    private void testEncryptDecrypt(String in, String pass) {
         String result = "";
         byte[] bytes = new byte[0];
 
         try {
-            String enc = encrypt(in, pass);
-            result = decrypt(enc, pass);
+            result = decrypt(encrypt(in, pass), pass);
         } catch (Exception e) { }
 
         try {
-            byte[] ct = encrypt(in.getBytes(UTF_8), pass.getBytes(UTF_8));
-            bytes = decrypt(ct, pass.getBytes(UTF_8));
+            bytes = decrypt(encrypt(in.getBytes(UTF_8), pass.getBytes(UTF_8)), pass.getBytes(UTF_8));
         } catch (Exception e) { }
 
         assertEquals(in, result, "Fail strings");
@@ -96,29 +73,17 @@ public class Aes256Test
     }
 
     @Test
-    public void testEncryptDecrypt1()
-    {
-        testEncryptDecrypt(
-            "Test! @#$%^&*",
-            "pass"
-        );
+    public void testEncryptDecrypt1() {
+        testEncryptDecrypt("Test! @#$%^&*", "pass");
     }
 
     @Test
-    public void testEncryptDecrypt2()
-    {
-        testEncryptDecrypt(
-            "Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺",
-            "pass"
-        );
+    public void testEncryptDecrypt2() {
+        testEncryptDecrypt("Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺", "pass");
     }
 
     @Test
-    public void testEncryptDecrypt3()
-    {
-        testEncryptDecrypt(
-            "Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺",
-            "哈罗 こんにちわ Акїў 😺"
-        );
+    public void testEncryptDecrypt3() {
+        testEncryptDecrypt("Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺", "哈罗 こんにちわ Акїў 😺");
     }
 }
--- java/tests/com/github/mervick/aes_everywhere/android/Aes256AndroidTest.java
+++ java/tests/com/github/mervick/aes_everywhere/android/Aes256AndroidTest.java
@@ -5,23 +5,19 @@ import com.github.mervick.aes_everywhere.Aes256Test;
 
 public class Aes256AndroidTest extends Aes256Test
 {
-    protected byte[] encrypt(byte[] in, byte[] pass) throws Exception
-    {
+    protected byte[] encrypt(byte[] in, byte[] pass) throws Exception {
         return Aes256.encrypt(in, pass);
     }
 
-    protected String encrypt(String in, String pass) throws Exception
-    {
+    protected String encrypt(String in, String pass) throws Exception {
         return Aes256.encrypt(in, pass);
     }
 
-    protected byte[] decrypt(byte[] in, byte[] pass) throws Exception
-    {
+    protected byte[] decrypt(byte[] in, byte[] pass) throws Exception {
         return Aes256.decrypt(in, pass);
     }
 
-    protected String decrypt(String in, String pass) throws Exception
-    {
+    protected String decrypt(String in, String pass) throws Exception {
         return Aes256.decrypt(in, pass);
     }
 }
