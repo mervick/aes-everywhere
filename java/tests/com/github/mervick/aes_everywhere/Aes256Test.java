2c8e81eb9b9a39fef071b7c2c3e8644c4f641b1e
Andrey Izman <izmanw@gmail.com>
2019-09-22 21:22:02 +0300

    Add tests

---
+++ java/tests/com/github/mervick/aes_everywhere/Aes256Test.java
@@ -0,0 +1,124 @@
+package com.github.mervick.aes_everywhere;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+
+public class Aes256Test
+{
+    protected byte[] encrypt(byte[] in, byte[] pass) throws Exception
+    {
+        return Aes256.encrypt(in, pass);
+    }
+
+    protected String encrypt(String in, String pass) throws Exception
+    {
+        return Aes256.encrypt(in, pass);
+    }
+
+    protected byte[] decrypt(byte[] in, byte[] pass) throws Exception
+    {
+        return Aes256.decrypt(in, pass);
+    }
+
+    protected String decrypt(String in, String pass) throws Exception
+    {
+        return Aes256.decrypt(in, pass);
+    }
+
+    private void testDecrypt(String in, String pass, String expect)
+    {
+        String result = "";
+        byte[] bytes = new byte[0];
+
+        try {
+            result = decrypt(in, pass);
+        } catch (Exception e) { }
+
+        try {
+            bytes = decrypt(in.getBytes(UTF_8), pass.getBytes(UTF_8));
+        } catch (Exception e) { }
+
+        assertEquals(expect, result, "Fail strings");
+        assertArrayEquals(expect.getBytes(UTF_8), bytes, "Fail bytes");
+    }
+
+    @Test
+    public void testDecrypt1() throws Exception
+    {
+        testDecrypt(
+            "U2FsdGVkX1+Z9xSlpZGuO2zo51XUtsCGZPs8bKQ/jYg=",
+            "pass",
+            "test"
+        );
+    }
+
+    @Test
+    public void testDecrypt2()
+    {
+        testDecrypt(
+            "U2FsdGVkX1+8b3WpGTbZHtd2T9PNQ+N7GqebGaOV3cI=",
+            "Data 😄 текст",
+            "test"
+        );
+    }
+
+    @Test
+    public void testDecrypt3()
+    {
+        testDecrypt(
+            "U2FsdGVkX18Kp+T3M9VajicIO9WGQQuAlMscLGiTnVyHRj2jHObWshzJXQ6RpJtW",
+            "pass",
+            "Data 😄 текст"
+        );
+    }
+
+    private void testEncryptDecrypt(String in, String pass)
+    {
+        String result = "";
+        byte[] bytes = new byte[0];
+
+        try {
+            String enc = encrypt(in, pass);
+            result = decrypt(enc, pass);
+        } catch (Exception e) { }
+
+        try {
+            byte[] ct = encrypt(in.getBytes(UTF_8), pass.getBytes(UTF_8));
+            bytes = decrypt(ct, pass.getBytes(UTF_8));
+        } catch (Exception e) { }
+
+        assertEquals(in, result, "Fail strings");
+        assertArrayEquals(in.getBytes(UTF_8), bytes, "Fail bytes");
+    }
+
+    @Test
+    public void testEncryptDecrypt1()
+    {
+        testEncryptDecrypt(
+            "Test! @#$%^&*",
+            "pass"
+        );
+    }
+
+    @Test
+    public void testEncryptDecrypt2()
+    {
+        testEncryptDecrypt(
+            "Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺",
+            "pass"
+        );
+    }
+
+    @Test
+    public void testEncryptDecrypt3()
+    {
+        testEncryptDecrypt(
+            "Test! @#$%^&*( 😆😵🤡👌 哈罗 こんにちわ Акїў 😺",
+            "哈罗 こんにちわ Акїў 😺"
+        );
+    }
+}
---
+++ java/tests/com/github/mervick/aes_everywhere/android/Aes256AndroidTest.java
@@ -0,0 +1,27 @@
+package com.github.mervick.aes_everywhere.android;
+
+import com.github.mervick.aes_everywhere.Aes256Test;
+
+
+public class Aes256AndroidTest extends Aes256Test
+{
+    protected byte[] encrypt(byte[] in, byte[] pass) throws Exception
+    {
+        return Aes256.encrypt(in, pass);
+    }
+
+    protected String encrypt(String in, String pass) throws Exception
+    {
+        return Aes256.encrypt(in, pass);
+    }
+
+    protected byte[] decrypt(byte[] in, byte[] pass) throws Exception
+    {
+        return Aes256.decrypt(in, pass);
+    }
+
+    protected String decrypt(String in, String pass) throws Exception
+    {
+        return Aes256.decrypt(in, pass);
+    }
+}
