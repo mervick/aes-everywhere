380abd60c0a8a04385ae3b158e9fb4992733b511
Andrey Izman <izmanw@gmail.com>
2019-11-12 03:19:30 +0200

    Add win version of gettimeofday

--- cpp/src/aes256.cpp
+++ cpp/src/aes256.cpp
@@ -38,6 +38,21 @@
 #include "md5.h"
 #include "base64.h"
 
+#if defined(_WIN32)
+#include <chrono>
+
+int gettimeofday(struct timeval* tp, struct timezone* tzp) {
+  namespace sc = std::chrono;
+  sc::system_clock::duration d = sc::system_clock::now().time_since_epoch();
+  sc::seconds s = sc::duration_cast<sc::seconds>(d);
+  tp->tv_sec = s.count();
+  tp->tv_usec = sc::duration_cast<sc::microseconds>(d - s).count();
+
+  return 0;
+}
+
+#endif // _WIN32
+
 /*****************************************************************************/
 /* Defines:                                                                  */
 /*****************************************************************************/
