7742e4005ad3225186debb1992728e59b9234357
Shadowsith <shadowsith94@gmail.com>
2020-02-03 23:32:46 +0100

    removed unused comment in aes256.ps1, adding MIT header to aes256.lua

--- lua/src/aes256.lua
+++ lua/src/aes256.lua
@@ -1,3 +1,31 @@
+-- aes256.lua
+-- This file is part of AES-everywhere project (https://github.com/mervick/aes-everywhere)
+--
+-- This is an implementation of the AES algorithm, specifically CBC mode,
+-- with 256 bits key length and PKCS7 padding.
+--
+-- Copyright Andrey Izman (c) 2018-2019 <izmanw@gmail.com>
+-- Copyright Philip Mayer (c) 2020 <philip.mayer@shadowsith.de>
+-- Licensed under the MIT license
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 -- Requires openssl command line program
 
 AES256 = {}
