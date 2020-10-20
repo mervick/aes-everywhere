9e534d9084450ce6b0e29f034ce31784064d203f
Andrey Izman <izmanw@gmail.com>
2020-10-20 16:56:02 +0300

    Update publish

--- python/.publish.sh
+++ python/.publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm dist/*
+pip3 install -U twine wheel setuptools
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
+python3 -m twine upload dist/* --verbose
 
