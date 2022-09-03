0969ffa99e6fbf8b53bd99c2cac2fc5dd7e2d7b1
Andrey Izman <izmanw@gmail.com>
2022-09-03 09:53:56 +0300

    Add decrypt()

--- dart/aeseverywhere/lib/aeseverywhere.dart
+++ dart/aeseverywhere/lib/aeseverywhere.dart
@@ -34,7 +34,7 @@ import 'dart:typed_data';
 import 'dart:math';
 
 class Aes256 {
-  static encrypt(String text, String passphrase) {
+  static String encrypt(String text, String passphrase) {
     final random = Random.secure();
     List<int> pass = passphrase.split('').map((ch) => ch.codeUnitAt(0)).toList();
     List<int> salt = List<int>.generate(8, (i) => random.nextInt(255));
@@ -55,18 +55,31 @@ class Aes256 {
 
     salted = 'Salted__'.split('').map((ch) => ch.codeUnitAt(0)).toList();
     List<int> bytes = [...salted, ...salt, ...encrypted];
-    return convert.base64.encode(bytes);
+    return convert.base64.encode(bytes).toString();
   }
 
-  static decrypt(String text, String passphrase) {
+  static String? decrypt(String encoded, String passphrase) {
+    List<int> enc = convert.base64.decode(encoded).toList();
+    final saltedStr = String.fromCharCodes(enc.sublist(0, 8).toList());
+    if (saltedStr != 'Salted__') return null;
 
-  }
+    List<int> pass = passphrase.split('').map((ch) => ch.codeUnitAt(0)).toList();
+    List<int> salt = enc.sublist(8, 16).toList();
+    List<int> text = enc.sublist(16).toList();
+    List<int> salted = [];
+    List<int> dx = [];
 
-  String _hex2bin(List<int> bytes) {
-    var charCodes = [];
-    for (var i = 0; i < bytes.length; i++) {
-      charCodes.add(String.fromCharCode(bytes[i]));
+    while (salted.length < 48) {
+      List<int> data = [...dx, ...pass, ...salt];
+      dx = md5.convert(data).bytes;
+      salted = [...salted, ...dx];
     }
-    return charCodes.join();
+
+    final key = Key(Uint8List.fromList(salted.sublist(0, 32)));
+    final iv = IV(Uint8List.fromList(salted.sublist(32, 48)));
+
+    final encryptor = Encrypter(AES(key, mode: AESMode.cbc));
+    final encrypted = Encrypted(Uint8List.fromList(text));
+    return encryptor.decrypt(encrypted, iv: iv);
   }
 }
