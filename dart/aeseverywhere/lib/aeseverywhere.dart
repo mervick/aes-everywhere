00cc1efd0979924afcacd5a8db0cc2456293bacc
Visarut Tippun <knotto.vt@gmail.com>
2024-07-20 12:37:37 +0700

    improving the readability and maintainability

--- dart/aeseverywhere/lib/aeseverywhere.dart
+++ dart/aeseverywhere/lib/aeseverywhere.dart
@@ -27,7 +27,7 @@
 
 library aeseverywhere;
 
-import 'dart:convert' as convert;
+import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
 
@@ -37,52 +37,50 @@ import 'package:encrypt/encrypt.dart';
 class Aes256 {
   Aes256._();
 
-  static String encrypt(String text, String passphrase) {
-    final random = Random.secure();
-    List<int> pass = passphrase.split('').map((ch) => ch.codeUnitAt(0)).toList();
-    List<int> salt = List<int>.generate(8, (i) => random.nextInt(255));
-    List<int> salted = [];
-    List<int> dx = [];
+  static List<int> _generateSaltedKeyAndIv(String passphrase, List<int> salt) {
+    final pass = utf8.encode(passphrase);
+    var dx = <int>[];
+    var salted = <int>[];
 
     while (salted.length < 48) {
-      List<int> data = [...dx, ...pass, ...salt];
+      final data = dx + pass + salt;
       dx = md5.convert(data).bytes;
-      salted = [...salted, ...dx];
+      salted.addAll(dx);
     }
+
+    return salted;
+  }
+
+  static String encrypt(String text, String passphrase) {
+    final random = Random.secure();
+    final salt = List<int>.generate(8, (_) => random.nextInt(256));
+    final salted = _generateSaltedKeyAndIv(passphrase, salt);
 
     final key = Key(Uint8List.fromList(salted.sublist(0, 32)));
     final iv = IV(Uint8List.fromList(salted.sublist(32, 48)));
-
     final encryptor = Encrypter(AES(key, mode: AESMode.cbc));
-    List<int> encrypted = encryptor.encrypt(text, iv: iv).bytes.toList();
 
-    salted = 'Salted__'.split('').map((ch) => ch.codeUnitAt(0)).toList();
-    List<int> bytes = [...salted, ...salt, ...encrypted];
-    return convert.base64.encode(bytes).toString();
+    final encrypted = encryptor.encrypt(text, iv: iv).bytes;
+    final saltedPrefix = utf8.encode('Salted__');
+    final result = saltedPrefix + salt + encrypted;
+
+    return base64.encode(result);
   }
 
   static String? decrypt(String encoded, String passphrase) {
-    List<int> enc = convert.base64.decode(encoded).toList();
-    final saltedStr = String.fromCharCodes(enc.sublist(0, 8).toList());
-    if (saltedStr != 'Salted__') return null;
+    final enc = base64.decode(encoded);
+    final saltedPrefix = utf8.decode(enc.sublist(0, 8));
 
-    List<int> pass = passphrase.split('').map((ch) => ch.codeUnitAt(0)).toList();
-    List<int> salt = enc.sublist(8, 16).toList();
-    List<int> text = enc.sublist(16).toList();
-    List<int> salted = [];
-    List<int> dx = [];
+    if (saltedPrefix != 'Salted__') return null;
 
-    while (salted.length < 48) {
-      List<int> data = [...dx, ...pass, ...salt];
-      dx = md5.convert(data).bytes;
-      salted = [...salted, ...dx];
-    }
+    final salt = enc.sublist(8, 16);
+    final text = enc.sublist(16);
+    final salted = _generateSaltedKeyAndIv(passphrase, salt);
 
     final key = Key(Uint8List.fromList(salted.sublist(0, 32)));
     final iv = IV(Uint8List.fromList(salted.sublist(32, 48)));
-
     final encryptor = Encrypter(AES(key, mode: AESMode.cbc));
-    final encrypted = Encrypted(Uint8List.fromList(text));
-    return encryptor.decrypt(encrypted, iv: iv);
+
+    return encryptor.decrypt(Encrypted(Uint8List.fromList(text)), iv: iv);
   }
 }
